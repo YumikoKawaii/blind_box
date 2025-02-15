7589241ee82f83293990f43fc75a266c1d49e120
Yumiko <yumiko.stl@gmail.com>
2025-02-15 12:57:09 +0700

    add express

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import express from "express"
+import {HTTP_PORT} from "./src/config/config.ts";
+
+const application = express()
+
+application.listen(HTTP_PORT, () => {
+    console.log(`Service serving at ${HTTP_PORT}`)
+})
